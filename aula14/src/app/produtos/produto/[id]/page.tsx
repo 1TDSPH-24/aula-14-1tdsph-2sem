39d6ffe35103b6dc21ee427dcdc79a556b0ce2b1
felipesora <felipeusora@gmail.com>
2024-09-26 08:53:43 -0300

    getall e getbyid

--- aula14/src/app/produtos/produto/[id]/page.tsx
+++ aula14/src/app/produtos/produto/[id]/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+import { TipoProduto } from "@/types";
+import { useEffect, useState } from "react";
 
 export default function Produto({params}:{params:{id:number}}) {
+  
+  const [produto, setProduto] = useState<TipoProduto[]>([])
+
+    useEffect(() => {
+        const chamadaApi = async () => {
+            const response = await fetch(`http://localhost:3000/api/base-produtos/${params.id}`);
+            const data = await response.json();
+            console.log(data);
+            setProduto(data);
+        }
+        chamadaApi();
+    }, []);
+  
   return (
     <div>
         <h2>Produto</h2>
