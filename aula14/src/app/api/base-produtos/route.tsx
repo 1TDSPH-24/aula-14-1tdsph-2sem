2dd3f02b866eb2d4815d359437602da0209eaf9f
lecristina <cristinale33@hotmail.com>
2024-09-26 09:35:15 -0300

    Teste de criação de dados com insomnia

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,3 +1,4 @@
+import { TipoProduto } from "@/types";
 import { promises as fs} from "fs"
 import { NextResponse } from "next/server";
 
@@ -7,4 +8,39 @@ export async function GET(){
     const produtos = JSON.parse(file);
 
     return NextResponse.json(produtos);
+}
+
+export async function POST(request:Request, response:Response){
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos = JSON.parse(file);
+
+    const {nome, preco, estoque} = await request.json();
+
+    // Criar um id no novo OBJETO que será inserido no base.json.
+    const produto = {nome, preco, estoque} as TipoProduto;
+    produto.id = (produtos[produtos.length-1].id + 1)
+
+    //Inserir  o n vo produto que já possui inclusive ID na lista de produtos:
+    produtos.push(produto);
+
+    //Processo de manipulção do arquivo com promises e process.cwd para colocar a lista no arquivo e atualizar ele
+
+    //1 - Convertendo a lista em String
+    const fileUpdate = JSON.stringify(produtos);
+    //2 - Escrevendo no arquivo através do caminho especificado
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+    return NextResponse.json(produto)
+
+}
+
+
+
+export async function PUT() {
+}
+
+export async function DELETE() {
+}
+
+export async function PATCH() {
 }
