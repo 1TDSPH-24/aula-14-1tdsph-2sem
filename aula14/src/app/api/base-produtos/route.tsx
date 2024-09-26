9c2ceb4695cc846fdf09e77b7c79121d1b98f14c
andrealtobelli <andrerogerio03@gmail.com>
2024-09-26 09:34:12 -0300

    Post com router handlers

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,4 +1,5 @@
-import { promises as fs} from "fs"
+import { TipoProduto } from "@/types";
+import { promises as fs, writeFile} from "fs"
 import { NextResponse } from "next/server";
 
 export async function GET(){
@@ -8,3 +9,34 @@ export async function GET(){
 
     return NextResponse.json(produtos);
 }
+
+
+export async function POST(request:Request, response:Response){
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos = JSON.parse(file);
+
+    const {nome, preco, estoque} = await request.json();
+
+    // Criar um id no novo OBJETO que será inserido no base.json.
+    const produto = {nome, preco, estoque} as TipoProduto;
+    produto.id = (produtos[produtos.length-1].id + 1)
+
+    //Inserir  o n vo produto que já possui inclusive ID na lista de produtos:
+    produtos.push(produto);
+
+    //Processo de manipulção do arquivo com promises e process.cwd para colocar a lista no arquivo e atualizar ele
+    
+    //1 - Convertendo a lista em String
+    const fileUpdate = JSON.stringify(produtos);
+    //2 - Escrevendo no arquivo através do caminho especificado
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+    return NextResponse.json(produto)
+
+}
+
+// export async function PUT(){}
+
+// export async function DELETE(){}
+
+// export async function PATCH(){}
