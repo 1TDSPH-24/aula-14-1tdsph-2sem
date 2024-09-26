e8e25906b55cc0c2d80bfc471c0f4c5dd9fb0a1b
cotete <feliperibeirots@gmail.com>
2024-09-26 09:34:30 -0300

    feito o post

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,3 +1,4 @@
+import { TipoProduto } from "@/types";
 import { promises as fs} from "fs"
 import { NextResponse } from "next/server";
 
@@ -8,3 +9,19 @@ export async function GET(){
 
     return NextResponse.json(produtos);
 }
+export async function POST(request:Request,response:Response){
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos : TipoProduto[] = JSON.parse(file);
+
+    const {nome,preco,estoque} = await request.json();
+    const produto = {nome,preco,estoque} as TipoProduto;
+    produto.id = (produtos[produtos.length-1].id + 1)
+
+    produtos.push(produto);
+
+    const fileUpdate = JSON.stringify(produtos)
+    await fs.writeFile(process.cwd() + '/src/data/base.json',fileUpdate);
+
+    return NextResponse.json(produto)
+}
+
