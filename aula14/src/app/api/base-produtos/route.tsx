199bfc9b03a0436291a20ec9ad8e7deec98506d8
felipesora <felipeusora@gmail.com>
2024-09-26 09:23:58 -0300

    POST com route handler

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,3 +1,4 @@
+import { TipoProduto } from "@/types";
 import { promises as fs} from "fs"
 import { NextResponse } from "next/server";
 
@@ -7,4 +8,29 @@ export async function GET(){
     const produtos = JSON.parse(file);
 
     return NextResponse.json(produtos);
+}
+
+export async function POST(request:Request, response:Response){
+
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos:TipoProduto[] = JSON.parse(file);
+
+    const {nome,preco,estoque} = await request.json();
+
+    // Criar um ID no novo OBJETO que será inserido no base.json
+    const produto = {nome,preco,estoque} as TipoProduto;
+    produto.id = ( produtos[produtos.length-1].id + 1 );
+
+    //Inserir o novo produto que ja possui inclusive ID na lista de produtos:
+    produtos.push(produto);
+
+    //Processo de manipulação do arquivo com promises e precess.cwd para colocar a lista no arquivo e atualizar ele.
+
+    // 1- Convertendo a lista em STRING
+    const fileUpdate = JSON.stringify(produtos);
+
+    // 2-Escrevendo no arquivo através do caminho especificado
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+    return NextResponse.json(produto);
 }
